5c1aa5e03163f72e755dcccc78ff61bdf3578787
gandrapukarthik <46021169+gandrapukarthik@users.noreply.github.com>
2021-12-17 17:11:57 +0530

    Add files via upload

--- security-groups.tf
+++ security-groups.tf
@@ -9,7 +9,7 @@ resource "aws_security_group" "worker_group_mgmt_one" {
     protocol  = "tcp"
 
     cidr_blocks = [
-      "10.0.0.0/8",
+      "172.20.0.0/16",
     ]
   }
 }
@@ -24,7 +24,7 @@ resource "aws_security_group" "worker_group_mgmt_two" {
     protocol  = "tcp"
 
     cidr_blocks = [
-      "192.168.0.0/16",
+      "172.20.0.0/16",
     ]
   }
 }
