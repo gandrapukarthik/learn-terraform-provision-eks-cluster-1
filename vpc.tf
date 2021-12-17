d545727477978211e4620e2e414de3fa84fb764c
gandrapukarthik <46021169+gandrapukarthik@users.noreply.github.com>
2021-12-17 17:11:08 +0530

    Add files via upload

--- vpc.tf
+++ vpc.tf
@@ -1,5 +1,5 @@
 variable "region" {
-  default     = "us-east-2"
+  default     = "ap-south-1"
   description = "AWS region"
 }
 
@@ -23,10 +23,10 @@ module "vpc" {
   version = "3.2.0"
 
   name                 = "education-vpc"
-  cidr                 = "10.0.0.0/16"
+  cidr                 = "172.20.0.0/16"
   azs                  = data.aws_availability_zones.available.names
-  private_subnets      = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets       = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+  private_subnets      = ["172.20.200.0/24", "172.20.210.0/24"]
+  public_subnets       = ["172.20.64.0/18", "172.20.128.0/18"]
   enable_nat_gateway   = true
   single_nat_gateway   = true
   enable_dns_hostnames = true
